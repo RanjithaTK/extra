78432a34ae39bb175af739c94a1318213618dc92
im ranjitha <ranjithatk101@gmail.com>
2022-05-28 21:15:43 +0530

    changes

--- LonelyInteger.js
+++ LonelyInteger.js
@@ -3,11 +3,11 @@ function LonelyInteger(array) {
   for (let i = 0; i < array.length; i++) {
     let count = 0
     for (let j = 0; j < array.length; j++) {
-      if (array[i] == array[j]) {
+      if (array[i] === array[j]) {
         count++
       }
     }
-    if (count == 1) {
+    if (count === 1) {
       return array[i]
     }
   }
